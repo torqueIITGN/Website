c779ef0645fdabb0087b5d2d561842f6cb9a324d
Chris Francis <chrisfrancischris@gmail.com>
2021-07-27 14:59:26 +0530

    Add adblocker CORS error handling message

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,13 @@
 
 
 function contributeSubmit() {
-    alert("Thank you for showing interest. We will get in touch with you soon.");
+    alert("Thank you for showing interest. We will get in touch with you soon.\n(Make sure you have turned off adblockers so that we can send the form data)");
 }
 
 function feedbackSubmit() {
-    alert("We received your feedback. Thanks a lot!");
+    alert("We received your feedback. Thanks a lot!\n(Make sure you have turned off adblockers so that we can send the form data)");
 }
 
 function subscribe() {
     window.location.href = 'https://forms.gle/ooeV6A3gxysKgsjh9';
 }
-
-window.onerror = function(message) { alert(message); return true; };
