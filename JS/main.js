a79d3e95c00af697313d6924cc29b2431e664a23
Chris Francis <chrisfrancischris@gmail.com>
2021-07-27 14:30:53 +0530

    Add code to handle forms

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+
+function contributeSubmit() {
+    alert("Thank you for showing interest. We will get in touch with you soon.");
+}
+
+function feedbackSubmit() {
+    alert("We received your feedback. Thanks a lot!");
+}
+
+function subscribe() {
+    window.location.href = 'https://forms.gle/ooeV6A3gxysKgsjh9';
+}
